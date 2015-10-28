c589385986bbd9b3127cd54826bd5df4992d9ab8
Mark Huot <mark@markhuot.com>
2015-10-28 13:58:49 -0400

    Update deploy.tutum.js

--- deploy.tutum.js
+++ deploy.tutum.js
@@ -101,7 +101,9 @@ for (var i=0; i<stack.services.length; i++) {
             var proxy = json[service.name].proxy[l].split('.');
             proxy.reverse();
             proxies['upstream.'+proxy.join('.')+':'+ports[k].inner_port] = ports[k].endpoint_uri;
-            unirest.post('https://my.slack.com/services/hooks/slackbot?token=PjM557gjIR6aftM1fcM52gk4&channel=%23int_davids_dev').send("Code was just deployed to "+json[service.name].proxy[l]).end();
+            unirest.post('https://my.slack.com/services/hooks/slackbot?token=PjM557gjIR6aftM1fcM52gk4&channel=%23int_davids_dev').send("Code was just deployed to "+json[service.name].proxy[l]).end(function(response) {
+              console.log(response.body);
+            });
           }
         }
       }
